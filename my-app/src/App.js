f4a46ac2feea2c5118a6155fac8109665ab713ed
Dhileepguru <dhileepguru497@gmail.com>
2025-01-03 07:57:20 +0530

    Created React app

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+   <h1 class="display-5">Hello world</h1>
+  );
+}
+
+export default App;
